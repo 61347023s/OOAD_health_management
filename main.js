87345e256b54052d310931f5f17f18780b5b3e5c
61347023s <61347023s@gapps.ntnu.edu.tw>
2025-04-18 09:30:34 +0800

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-const loggedInUsername = localStorage.getItem('loggedInUsername');
-if (loggedInUsername) {
-    document.getElementById('welcomeMsg').textContent = `Your Grace, ${loggedInUsername}!`;
-} else {
-    console.log('User not logged in.');
-}
